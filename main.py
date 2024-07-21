43b4cceb76fc1ae51749b068556faaf4108f64ac
Akash-nath29 <anath5440@gmail.com>
2024-07-21 17:26:21 +0530

    Add Both Random Forest Regressor and Linear Regression

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ import pandas as pd
 
 # Create an instance of CustomTraining
 custom_training = CustomTraining('replicated_data.csv')
-score = custom_training.model_training()
+score = custom_training.train_random_forest_regressor()
+
+print("Score: ", str(score))
+
+gpa = custom_training.model_prediction([[1700]])
+print("GPA: ", str(gpa[0]))
+
+custom_training = CustomTraining('replicated_data.csv')
+score = custom_training.train_linear_regression()
 
 print("Score: ", str(score))
 
--- utils/custom_training.py
+++ utils/custom_training.py
@@ -1,7 +1,7 @@
 import pickle
 import pandas as pd
 from sklearn.model_selection import train_test_split
-# from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import r2_score
@@ -24,11 +24,26 @@ class CustomTraining:
         
         return X_train, y_train, X_test, y_test
     
-    def model_training(self):
+    def train_random_forest_regressor(self):
         X_train, y_train, X_test, y_test = self.data_preparation()
         
         # Use RandomForestRegressor instead of LinearRegression
         model = RandomForestRegressor(n_estimators=100, random_state=42)
+        model.fit(X_train, y_train)
+        
+        # Save the model
+        with open('model.pkl', 'wb') as f:
+            pickle.dump(model, f)
+        
+        # Evaluate the model
+        y_pred = model.predict(X_test)
+        
+        return r2_score(y_test, y_pred)
+    
+    def train_linear_regression(self):
+        X_train, y_train, X_test, y_test = self.data_preparation()
+        
+        model = LinearRegression()
         model.fit(X_train, y_train)
         
         # Save the model
